b9f3619b2aa6e97b74dfa937f8abc425f5f03511
CurtMinyard <cminyard@perseverenow.org>
2025-06-30 15:28:49 -0500

    Initial commit

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
-
+const tock = new Audio('sounds/tock.mp3');
+let count = 1;
 // This function is called every 600ms
 function update() {
+    if (count === 4) {
+        tock.play();
+    } else {
+        tick.play();
+    }
 
-    // Play the 'tick' sound
-    tick.play();
+    // Move to next beat (loop back to 1 after 4)
+    count = count % 4 + 1;
+}
+    
+
+function updatetock() {
+
+    // Play the 'tock' sound
+    tock.play();
+    
 }
 
 // This function sets up update() to be called every 600ms
@@ -15,3 +29,4 @@ function setupUpdate() {
 
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
+
